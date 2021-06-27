44afedda4200243cddf2e2fb8e4bcaaed7aee5e6
yann <yanndubois4@gmail.com>
2021-06-27 20:47:30 +0200

    test : add selectors test

--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -7,7 +7,8 @@ const expenses = [
     description: "Gum",
     note: "",
     amount: 195,
-    createdAt: 0,
+    createdAt: moment(0).valueOf(),
+    endDate: moment(0).subtract(4, "days").valueOf(),
   },
   {
     id: "2",
@@ -15,13 +16,15 @@ const expenses = [
     note: "",
     amount: 19500,
     createdAt: moment(0).subtract(4, "days").valueOf(),
+    endDate: moment(0).subtract(3, "days").valueOf(),
   },
   {
     id: "3",
     description: "Credit Card",
     note: "",
+    createdAt: moment(0).add(5, "days").valueOf(),
     amount: 4500,
-    createdAt: moment(0).add(4, "days").valueOf(),
+    endDate: undefined,
   },
 ];
 
@@ -34,7 +37,7 @@ test("should filter by text value", () => {
   };
 
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[1], expenses[2]]);
+  expect(result).toEqual([expenses[2], expenses[1]]);
 });
 
 test("should filter by text startDate", () => {
@@ -46,5 +49,37 @@ test("should filter by text startDate", () => {
   };
 
   const result = selectExpenses(expenses, filters);
-  expect(result).toEqual([expenses[0], expenses[2]]);
+  expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test("should filter by text endDate", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+    startDate: undefined,
+    endDate: moment(0).add(2, "days"),
+  };
+
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test("should sort by date", () => {
+  const filters = {
+    text: "",
+    sortBy: "date",
+  };
+
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test("should sort by amount", () => {
+  const filters = {
+    text: "",
+    sortBy: "amount",
+  };
+
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
 });
